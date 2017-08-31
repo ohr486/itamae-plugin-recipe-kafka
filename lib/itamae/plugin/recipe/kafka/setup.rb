83330fbf87f78566ad22e41eb87dcb607e43e239
ohr486 <ohr486@gmail.com>
2017-08-31 14:23:58 +0900

    add conf

--- lib/itamae/plugin/recipe/kafka/setup.rb
+++ lib/itamae/plugin/recipe/kafka/setup.rb
@@ -2,11 +2,29 @@
 
 kafka_version = node[:kafka][:version]
 kafka_scala_version = node[:kafka][:kafka_scala_version]
+kafka_config = node[:kafka][:config]
 
 directory "/usr/local/kafka" do
   mode "0755"
   owner "root"
   group "root"
+end
+
+directory "/usr/local/kafka/config" do
+  mode "0755"
+  owner "root"
+  group "root"
+end
+
+kafka_config[:kafka].keys.each do |host|
+  template "/usr/local/kafka/config/server.properties-#{host}" do
+    action :create
+    source "templates/server.properties.erb"
+    owner "root"
+    group "root"
+    mode "644"
+    variables(host: host, kafka_config: kafka_config)
+  end
 end
 
 template "/etc/profile.d/kafka.sh" do
--- spec/kafka_spec.rb
+++ spec/kafka_spec.rb
@@ -4,3 +4,28 @@ describe file("/etc/profile.d/kafka.sh") do
   it { should be_file }
   its(:content) { should match /2\.12-0\.11\.0\.0/ }
 end
+
+describe file("/usr/local/kafka/config") do
+  it { should be_directory }
+end
+
+describe file("/usr/local/kafka/config/server.properties-host1") do
+  it { should be_file }
+  its(:content) { should contain("broker.id=1") }
+  its(:content) { should contain("listeners=PLAINTEXT://host.broker1:9091") }
+  its(:content) { should contain("zookeeper.connect=host.zookeeper:2181") }
+end
+
+describe file("/usr/local/kafka/config/server.properties-host2") do
+  it { should be_file }
+  its(:content) { should contain("broker.id=2") }
+  its(:content) { should contain("listeners=PLAINTEXT://host.broker2:9092") }
+  its(:content) { should contain("zookeeper.connect=host.zookeeper:2181") }
+end
+
+describe file("/usr/local/kafka/config/server.properties-host3") do
+  it { should be_file }
+  its(:content) { should contain("broker.id=3") }
+  its(:content) { should contain("listeners=PLAINTEXT://host.broker3:9093") }
+  its(:content) { should contain("zookeeper.connect=host.zookeeper:2181") }
+end
