15b559256623a03a861cc163c56de1c7eb74366a
ohr486 <ohr486@gmail.com>
2017-12-05 12:23:02 +0900

    fix svrspc

--- spec/kafka_spec.rb
+++ spec/kafka_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe file("/etc/profile.d/kafka.sh") do
   it { should be_file }
-  its(:content) { should match /2\.12-0\.11\.0\.0/ }
+  its(:content) { should match /2\.12-1\.0\.0/ }
 end
 
 describe file("/usr/local/kafka/config") do
